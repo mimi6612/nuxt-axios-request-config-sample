012492b011efc45ccdf0eb2695c5bf98ba4a6875
ryoichiroSlogan <komatsuzaki.ryoichiro@slogan.jp>
2020-10-10 22:22:31 +0900

    sample app

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <h1>Hello world!</h1>
+  </main>
+</template>
+<script>
+import axios from "axios";
+axios.defaults.headers.common["default-header"] = "default-header";
+
+export default {
+  async fetch({ $axios }) {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1", {
+        headers: { header1: "header1" },
+      })
+      .then((response) => {
+        console.error(response.config);
+      });
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1", {
+        headers: { header1: "header1" },
+      })
+      .then((response) => {
+        console.error(response.config);
+      });
+  },
+};
+</script>
